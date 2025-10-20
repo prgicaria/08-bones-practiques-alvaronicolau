6d9b9df402e64827efb86003c9ca130eb2a3a588
alvaronicolau <anicolau@instituticaria.cat>
2025-10-20 08:42:46 +0200

    :)

--- bones_practiques.py
+++ bones_practiques.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 
-'''Bones pràctiques. Es divideixen dos nombres enters 
-i es mosta la divisió, el quocient i el residu.
+'''Bones pràctiques.
+Es divideixen dos nombres enters: es mostra la divisió, el quocient i el residu.
 
 Institut Icària
 Programació  - 1r Batxillerat - Curs 2025-26
 
 El programa demana a l'usuari dos nombres enters, dividend i divisor.
 La sortida per pantalla mostra la divisió, el quocient i el residu.
-
 '''
 
 __author__ = "Álvaro"
 __email__ = "anicolau@instituticaria.cat"
 __date__ = "2025/10/17"
 
-dividend = int(input("Entra el dividend: "))
-divisor = int(input("Entra el divisor: "))
+dividend = int(input())
+divisor = int(input())
 
 quocient = dividend // divisor
 residu = dividend % divisor
